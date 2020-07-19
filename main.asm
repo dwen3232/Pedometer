0c3a3220650e904d5243d8101af77ad2fa40202b
David Wen <dwen3232@gmail.com>
2020-07-19 19:26:04 -0400

    peak detection completed

--- main.asm
+++ main.asm
@@ -10,14 +10,13 @@ ORG 0
 	STORE  ReadCount   ; Initialize counter for display
 	
 ReadLoop:
-	CALL   WaitForData ; Subroutine to wait for new data
+	CALL    WaitForData ; Subroutine to wait for new data
 	; (The subroutine doesn't do anything yet. It just returns immediately.)
 	
-	; Increment the acquisition counter and display it	
-	; LOAD   ReadCount
-	; ADDI   1
-	; STORE  ReadCount
-	; OUT    Hex1
+	; Increment the acquisition counter
+	LOAD   ReadCount
+	ADDI   1
+	STORE  ReadCount
 
 	CALL	ReadX       ; Get the X acceleration data
 	CALL	Square
@@ -33,71 +32,118 @@ ReadLoop:
 	ADD		X_squared
 	ADD		Y_squared
 	ADD		Z_squared
+	SUB     NormConst
 	STORE	Mag_squared
 	
-	OUT		Hex0        ; Display unfiltered data
 	CALL	Filter      ; Calculate moving average
+	OUT		Hex0        ; Display unfiltered data
 	; Manipuate the data to create a display on the LEDs.
-	CALL   BarGraph
-
-	JUMP   ReadLoop    ; Repeat forever
-	
-X_squared: DW 0
-Y_squared: DW 0
-Z_squared: DW 0
+	; CALL    BarGraph
+	CALL	DetectPeak
+	JUMP    ReadLoop    ; Repeat forever
+X_squared: 	   DW 0
+Y_squared: 	   DW 0
+Z_squared: 	   DW 0
 Mag_squared:   DW 0
 
-; Used for peak detection
-StartVec0: DW 0
-StartVec1: DW 0
-StartVec2: DW 0 
-EndVec0:  DW 0
-EndVec1:  DW 0
-EndVec2:  DW 0
 
 DetectPeak:
-	LOAD StartVec0
-	SUB  StartVec1
-	JPOS NoPeak
-
-	LOAD StartVec1
-	SUB  StartVec2
-	JPOS NoPeak
-
-	LOAD StartVec2
-	SUB  EndVec0
-	JNEG NoPeak
-
-	LOAD EndVec0
-	SUB  EndVec1
-	JPOS NoPeak
-
-	LOAD EndVec1
-	SUB  EndVec2
-	JPOS NoPeak
-
-	JUMP PeakDetected ; TODO handling peak detected
-
-NoPeak: ; Shift everything down
-	LOAD StartVec1
-	STORE StartVec0
+	; check if we are in a peak currently
+	LOAD	InPeakBoolean
+	JPOS	InPeak
+UpdateStartVec:
+	; update StartVec aquisition values and timestamps
+	LOAD	StartVec1
+	STORE	StartVec0
+	LOAD	StartTime1
+	STORE	StartTime0
 	
-	LOAD StartVec2
-	STORE StartVec1
-
-	LOAD EndVec0
-	STORE StartVec2
-
-	LOAD EndVec1
-	STORE EndVec0
-
-	LOAD EndVec2
-	STORE EndVec1
-
-	RETURN
+	LOAD	StartVec2
+	STORE	StartVec1
+	LOAD	StartTime2
+	STORE	StartTime1
 	
-PeakDetected:
+	LOAD	Mag_squared
+	STORE	StartVec2
+	LOAD	ReadCount
+	STORE	StartTime2
+	
+	; check if not increasing - branch to EndDetectPeak if true
+	LOAD 	StartVec0
+	SUB  	StartVec1
+	JPOS 	EndDetectPeak
+
+	LOAD 	StartVec1
+	SUB  	StartVec2
+	JPOS 	EndDetectPeak
+	
+	; check if most recent value is above threshold - branch to EndDetectPeak if below
+	LOAD	StartVec2
+	SUB		Threshold
+	JNEG	EndDetectPeak
+	; set InPeakBoolean to true if most recent value is above threshold
+	LOADI	1
+	STORE	InPeakBoolean
+InPeak:
+	; update MaxPeakValue
+	LOAD	Mag_squared
+	SUB		MaxPeakValue
+	JNEG	UpdateEndVec
+	LOAD 	Mag_squared
+	STORE	MaxPeakValue
+UpdateEndVec:
+	; update EndVec aquisition values and timestamps
+	LOAD	EndVec1
+	STORE	EndVec0
+	LOAD	EndTime1
+	STORE	EndTime0
+	
+	LOAD	Mag_squared
+	STORE	EndVec1
+	LOAD	ReadCount
+	STORE	EndTime1
+	
+	; check if EndVec is not increasing - branch to EndDetectPeak if true
+	LOAD	EndVec0
+	SUB		EndVec1
+	JPOS	EndDetectPeak
+	; check if most recent value is above threshold - branch to EndDetectPeak if above
+	LOAD	EndVec1
+	SUB		Threshold
+	JPOS	EndDetectPeak
+	; set InPeakBoolean to false if most recent value is below threshold
+	LOADI	0
+	STORE	InPeakBoolean
+	; increment StepCount and calculate time in peak
+	LOAD	StepCount
+	ADDI	1
+	STORE	StepCount
+	OUT		Hex1
+	
+	LOAD	EndTime1
+	SUB		StartTime0
+	STORE	TimeInPeak
+EndDetectPeak:
+	; subroutine ends here
 	RETURN
+
+; Used for peak detection
+StartVec0: 	   DW 0
+StartTime0:	   DW 0
+StartVec1: 	   DW 0
+StartTime1:	   DW 0
+StartVec2: 	   DW 0
+StartTime2:	   DW 0 
+
+EndVec0:	   DW 0
+EndTime0:	   DW 0
+EndVec1:	   DW 0
+EndTime1:	   DW 0
+
+InPeakBoolean: DW 0
+MaxPeakValue:  DW 0
+TimeInPeak:	   DW 0
+
 ; WaitForData will poll the ADXL345 until it responds that there is fresh data.
 ; Once this returns, you can read the accelerometer data and know that you
 ; aren't reading the same data more than once.
@@ -310,7 +356,7 @@ I2CError:
 	ADDI   &H12C       ; "I2C"
 	OUT    Hex0        ; display error message
 	JUMP   I2CError
-	
+Temp:      DW 0
 	
 ;*******************************************************************************
 ; Abs: 2's complement absolute value
@@ -348,7 +394,6 @@ UpdateSquare:
 	JUMP  UpdateSquare
 SquareFound:
 	LOAD  SquareVal
-	;CALL  Abs
 	RETURN
 SquareInput: DW 0
 Index:		 DW 0
@@ -369,20 +414,20 @@ AccCfg: ; List of commands to send the ADXL345 at startup
 	DW &H0000      ; Meaningless write to sync communication
 	DW &H3101      ; Right-justified 10-bit data, +/-2 G
 	DW &H3800      ; No FIFO
-	DW &H2C0A      ; 6.25 samples per second
+	DW &H2C08      ; 6.25 samples per second
 	DW &H2D08      ; No sleep
 
 
 
 ; Variables
-Temp:      DW 0
+StepCount: DW 0
 ReadCount: DW 0
 Score:     DW 0
 
 ; Useful values
 Zero:      DW 0
 NegOne:    DW -1
-One:
+One:	   DW 1
 Bit0:      DW &B0000000001
 Bit1:      DW &B0000000010
 Bit2:      DW &B0000000100
@@ -395,6 +440,8 @@ Bit8:      DW &B0100000000
 Bit9:      DW &B1000000000
 LoByte:    DW &H00FF
 HiByte:    DW &HFF00
+NormConst: DW &H3000	;experimental value
+Threshold: DW &H4000	;experimental value
 
 ; Filter variables
 X0:         DW 0
